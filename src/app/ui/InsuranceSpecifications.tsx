e8d796833768c8aef8989564d2749ea9ebcd4434
Amir Gholikhani <amirgholikhani70@gmail.com>
2025-05-19 16:26:25 +0330

    Add car info section

--- src/app/ui/InsuranceSpecifications.tsx
+++ src/app/ui/InsuranceSpecifications.tsx
@@ -50,6 +50,20 @@ export default function InsuranceSpecifications() {
         </div>
         <div className="flex w-[55px] items-center justify-center border-2 border-l-0 rounded-r-[5px] font-[500] text-[18px]">{thirdNumber}</div>
       </div>
+      <div className="flex flex-col self-center gap-2 w-[280px]">
+        <div className="flex justify-between items-center">
+          <h3 className="font-[400] text-[14px] text-[#808080]">شرکت بیمه گر</h3>
+          <h3 className="font-[400] text-[14px]">{insurerCompany}</h3>
+        </div>
+        <div className="flex justify-between items-center">
+          <h3 className="font-[400] text-[14px] text-[#808080]">برند خودرو</h3>
+          <h3 className="font-[400] text-[14px]">{brand}</h3>
+        </div>
+        <div className="flex justify-between items-center">
+          <h3 className="font-[400] text-[14px] text-[#808080]">مدل خودرو</h3>
+          <h3 className="font-[400] text-[14px]">{model}</h3>
+        </div>
+      </div>
     </div>
   )
 }
