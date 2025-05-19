86b7edc39a013399efc83b64e783f3c7c5943426
Amir Gholikhani <amirgholikhani70@gmail.com>
2025-05-19 15:09:31 +0330

    Add Iransans fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import AppLayout from "@/app/AppLayout";
+import '@/assets/fonts/WebFonts/css/style.css'
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
